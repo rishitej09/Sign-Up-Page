293035d05db41a00add61d6f4c78484235c5a956
rishitej09 <rishitej0909gmail.com>
2022-05-21 18:07:01 +0530

    Complete SignUpPage

---
+++ app.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const bodyparser = require("body-parser");
+const request = require("request");
+const https = require("https");
+const { options } = require("request");
+const { json } = require("express/lib/response");
+
+const app = express();
+
+app.use(bodyparser.urlencoded({extended:true}));
+
+app.use(express.static("public"));
+
+app.get( "/" , function (req , res){
+    res.sendFile(__dirname+"/signUp.html");
+});
+
+
+app.post("/" , function(req , res){
+    const fname = req.body.fname;
+    const lname = req.body.lname;
+    const email = req.body.email;
+
+    const data = {
+        members:[
+            {
+                email_address:email,
+                status:"subscribed",
+                merge_fields:{
+                    FNAME:fname,
+                    LNAME:lname
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us13.api.mailchimp.com/3.0/lists/bb54a7ab61";
+
+    const options = {
+        method:"post",
+        auth : "Rishi:0b79ef6bd997c265d06823e078b5c978-us13"
+    }
+    const request = https.request(url , options , function(response){
+
+        if (response.statusCode===200){
+        res.sendFile(__dirname+"/success.html");
+        }else{
+        res.sendFile(__dirname+"/failure.html");
+        };
+
+        response.on("data" , function(data){
+            console.log(JSON.parse(data));
+        });
+    });
+    request.write(jsonData);
+    request.end();
+});
+
+app.post("/failure" , function(req , res){
+    res.redirect("/");
+});
+
+
+
+app.listen( 3000 || process.env.PORT , function( req , res ){
+    console.log("Server is Running At Port 3000");
+});
+
+
+
+
